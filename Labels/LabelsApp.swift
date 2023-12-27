40e9fc812d4ca044518083f4e15b0e749cba00cf
Edwin <edwin12sv@gmail.com>
2023-12-26 22:39:00 -0600

    Initial Commit

---
+++ Labels/LabelsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LabelsApp.swift
+//  Labels
+//
+//  Created by elsalvador on 26/12/23.
+//
+
+import SwiftUI
+
+@main
+struct LabelsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
